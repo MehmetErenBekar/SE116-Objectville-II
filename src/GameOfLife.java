065a5e4dfb820c20b8af3696c528af37a3a53445
iremmolenaar <irem_molenaar@hotmail.com>
2026-05-22 15:37:57 +0300

    game of life upgrade

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,14 +1,14 @@
 public class GameOfLife {
     // checks if the parcel is alive or not
     // true means it is alive false means it is dead
-    public boolean apply(boolean currentStatus, int neighborCount){
+    public static boolean apply(boolean currentStatus, int neighborCount){
         if(currentStatus){
             if(neighborCount == 2 || neighborCount == 3){
                 return true;
             }else if(neighborCount > 3){
                 return false;
             }
-        }else if(currentStatus==false){
+        }else{
             if(neighborCount == 3){
                 return true;
             }
