cc45143b7d0ac20f1ce51e409303b82809809a56
projectsample1 <senabygn7@gmail.com>
2026-05-24 15:25:49 +0300

    Add UtilityProvider, PowerPlant, WaterPumpingStation, InternetHub classes

--- src/com/objectville/model/PowerPlant.java
+++ src/com/objectville/model/PowerPlant.java
@@ -1,4 +1,12 @@
 package com.objectville.model;
 
 public class PowerPlant extends UtilityProvider{
+    public PowerPlant(int row, int col) {
+        super(row, col);
+        this.type = "ELECTRICITY";
+    }
+    @Override
+    public char display(){
+        return 'P';//P means power
+    }
 }
--- src/com/objectville/model/UtilityProvider.java
+++ src/com/objectville/model/UtilityProvider.java
@@ -1,7 +1,27 @@
 package com.objectville.model;
 
-public abstract class UtilityProvider extends Cell
+public abstract class UtilityProvider extends Cell {
+    protected String type; //electricity , water or internet
+    protected int capacity=100; // each provider generates 100 units per tick
 
+    public UtilityProvider(int row,int col){
+        super(row, col);
+    }
 
-{
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
 }
